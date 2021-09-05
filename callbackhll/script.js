1d961eb1a23466f5e74ac4868686400ab248d52d
pradeep1331 <64856783+pradeep1331@users.noreply.github.com>
2021-09-05 10:14:46 +0530

    Add files via upload

---
+++ callbackhll/script.js
@@ -0,0 +1,60 @@
+
+const pTag = document.querySelector("#para1");
+
+function callbackhll(a){
+   pTag.innerHTML = a;
+           function callbackhll(a){
+            pTag.innerHTML = a;
+
+              function callbackhll(a){
+               pTag.innerHTML = a;
+               function callbackhll(a){
+                  pTag.innerHTML = a;
+                  function callbackhll(a){
+                     pTag.innerHTML = a;
+
+                     function callbackhll(a){
+                        pTag.innerHTML = a;
+                        function callbackhll(a){
+                           pTag.innerHTML = a;
+                           function callbackhll(a){
+                              pTag.innerHTML = a;
+                              function callbackhll(a){
+                                 pTag.innerHTML = a;
+                                 function callbackhll(a){
+                                    pTag.innerHTML = a;
+                                    function callbackhll(a){
+                                       pTag.innerHTML = a;                                   
+                                       
+                                    }
+                                   
+                                    setTimeout(() => {callbackhll(a-1); }, 1000);                          
+                                 }
+                                
+                                 setTimeout(() => {callbackhll(a-1); }, 1000);                                 
+                              }
+                             
+                              setTimeout(() => {callbackhll(a-1); }, 1000); 
+                            }
+                          
+                            setTimeout(() => {callbackhll(a-1); }, 1000); 
+                        }
+                       
+                        setTimeout(() => {callbackhll(a-1); }, 1000); 
+                     }
+                    
+                     setTimeout(() => {callbackhll(a-1); }, 1000); 
+                  }
+                 
+                  setTimeout(() => {callbackhll(a-1); }, 1000); 
+               }
+              
+               setTimeout(() => {callbackhll(a-1); }, 1000);                 
+            }
+          
+            setTimeout(() => {callbackhll(a-1); }, 1000);              
+          }
+          setTimeout(() => {callbackhll(a-1); }, 1000); 
+}
+
+setTimeout(() => {callbackhll(10); }, 1000); 
