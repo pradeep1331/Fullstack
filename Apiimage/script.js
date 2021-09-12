37a100cbca8f5c133442700457b9b5d0dc7a9e29
pradeep1331 <64856783+pradeep1331@users.noreply.github.com>
2021-09-12 10:47:15 +0530

    Add files via upload

---
+++ Apiimage/script.js
@@ -0,0 +1,16 @@
+const containerTag = document.getElementById("container");
+
+fetch("https://jsonplaceholder.typicode.com/photos")
+.then((response)=>response.json())
+.then((data)=>{
+    
+    
+    for(var i=0;i<50;i++){
+    var imgUrl = data[i].url;
+    var title = data[i].title.slice(0,30);
+    const html =  `<div class="items"><p>${title}</p><img src="${imgUrl}" width="100" heigth="100"><br><div>`;
+  containerTag.insertAdjacentHTML("afterbegin",html);
+
+    }
+  
+})
